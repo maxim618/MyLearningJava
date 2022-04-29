2202996e0cd0f53da2588e4a4f630471d7d8bc70
MaximButmanov <but-maxim@yandex.ru>
2022-04-29 15:01:53 +0300

    init commit

---
+++ SpringExperience3/src/main/java/org/example/JavaDevelopment.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class JavaDevelopment implements Development{
+
+    @Override
+    public String getName() {
+        return "Alexa";
+    }
+
+    @Override
+    public String getJob() {
+        return "Middle Java developer";
+    }
+}
