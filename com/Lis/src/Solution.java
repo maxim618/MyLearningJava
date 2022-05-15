24c8c414a31a3e111f5f024b07229db74d9ec1d7
MaximButmanov <but-maxim@yandex.ru>
2022-05-15 18:08:05 +0300

    init commit

---
+++ com/Lis/src/Solution.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public static class Fox {
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
