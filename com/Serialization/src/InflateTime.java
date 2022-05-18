02e6942667cdfa762d495fac85a637b332123e43
MaximButmanov <but-maxim@yandex.ru>
2022-05-18 18:15:10 +0300

    init commit

---
+++ com/Serialization/src/InflateTime.java
@@ -0,0 +1,34 @@
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class InflateTime {
+    public static void main(String [] args)
+    {
+        String filename = "time.ser";
+        if(args.length > 0) {
+            filename = args[0];
+        }
+        PersistentTime time = null;
+
+        FileInputStream fis = null;
+        ObjectInputStream in = null;
+
+           try {
+                fis = new FileInputStream(filename);
+                in = new ObjectInputStream(fis);
+                time = (PersistentTime)in.readObject();
+                in.close();
+               }
+           catch(IOException | ClassNotFoundException ex) {
+                 ex.printStackTrace();
+           }
+        // распечатать восстановленное время
+        assert time != null;
+        System.out.println("Время разложения: " + time.getTime());
+             System.out.println();
+              // распечатать текущее время
+             System.out.println("Текущее время: " + Calendar.getInstance().getTime());
+           }
+}
