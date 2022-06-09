a55c84a0d07d471189ae61ece71550ed18c10f1f
MaximButmanov <but-maxim@yandex.ru>
2022-06-09 18:51:36 +0300

    init commit

---
+++ SpringExperience3/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.example.HiringDepartment;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String ... args){
+        //Определяем контекст файл в котором содержатся прописанные нами бины
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //Получем бины, которые были определены в файле applicationContext.xml
+        HiringDepartment hiringDepartment = context.getBean("hiringDepartment", HiringDepartment.class);
+
+        hiringDepartment.displayInfo();
+        //System.out.println("Citizenship: " + hiringDepartment.getCitizenship());
+       //System.out.println("Salary: " + hiringDepartment.getSalary());
+        context.close(); //Контекст всегда должен закрываться
+    }
+}
