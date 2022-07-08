7e151f894cf8dc6fe628cfbe4b0c9c42bdc6eaf3
MaximButmanov <but-maxim@yandex.ru>
2022-07-08 19:35:55 +0300

    refactor Main

--- circularArray/src/Main.java
+++ circularArray/src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
+
     public static void main(String[] args) {
         int[] a = new int[]{ 1, 2, 3,
                 4, 5, 6 };
         int n = 6;
         print(a, n, 1);
     }
+
     public static void print(int a[], int n, int ind){
         // Создаем вспомогательный массив
         // двойного размера.
