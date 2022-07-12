c76c46551392401435ee12b3b0740493d56440fe
MaximButmanov <but-maxim@yandex.ru>
2022-07-12 19:55:23 +0300

    MyLearningSpring2906Application.java

---
+++ MyLearningSpring29_06/src/main/java/com/example/MyLearningSpring29_06/MyLearningSpring2906Application.java
@@ -0,0 +1,13 @@
+package com.example.MyLearningSpring29_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearningSpring2906Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLearningSpring2906Application.class, args);
+	}
+
+}
