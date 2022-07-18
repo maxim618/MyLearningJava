6c99eaa67fc024ecedef38c6f559574945c08d6c
MaximButmanov <but-maxim@yandex.ru>
2022-07-18 19:56:56 +0300

    MyLearningSpring2906ApplicationTests.java

---
+++ MyLearningSpring29_06/src/test/java/com/example/MyLearningSpring29_06/MyLearningSpring2906ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyLearningSpring29_06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLearningSpring2906ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
