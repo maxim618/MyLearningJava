e5c03c2f5077f5817c909173bfec7eb11c3ce3d1
MaximButmanov <but-maxim@yandex.ru>
2022-09-08 20:34:45 +0300

    init commit

---
+++ SpringExperience3/src/main/java/org/example/PythonDevelopment.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class PythonDevelopment implements Development{
+    @Override
+    public String getName() {
+        return "Mike";
+    }
+
+    @Override
+    public String getJob() {
+        return "Middle Python developer";
+    }
+}
