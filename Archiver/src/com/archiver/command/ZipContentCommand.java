7a64e1887779f0a1a37677e13b5fdf533e3d6c83
MaximButmanov <but-maxim@yandex.ru>
2022-09-17 20:43:27 +0300

    init commit

---
+++ Archiver/src/com/archiver/command/ZipContentCommand.java
@@ -0,0 +1,26 @@
+package com.archiver.command;
+
+
+import com.archiver.ConsoleHelper;
+import com.archiver.FileProperties;
+import com.archiver.ZipFileManager;
+
+import java.util.List;
+
+public class ZipContentCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
+        }
+
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
+    }
+}
