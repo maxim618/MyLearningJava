7b6e4a3c7a223ca8d1510dfc6f2f7dbfb009d113
MaximButmanov <but-maxim@yandex.ru>
2022-10-14 21:17:55 +0300

    init commit

---
+++ 7WaysToCreateImmutableObjects/The7Ways/src/main/java/fourth/Person.java
@@ -0,0 +1,14 @@
+package fourth;
+
+public class Person implements  ImmutablePerson{
+    private String name;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
