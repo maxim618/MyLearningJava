117c1499ca0006f06ac72b33e736b2edea8fea25
MaximButmanov <but-maxim@yandex.ru>
2022-11-17 16:04:07 +0300

    init commit

---
+++ hibernate-starter/src/test/java/com/dmdev/HibernateRunnerTest.java
@@ -0,0 +1,86 @@
+package com.dmdev;
+
+import com.dmdev.entity.Birthday;
+import com.dmdev.entity.User;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HibernateRunnerTest {
+	@Test
+	void CheckGetReflectionApi() throws SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = preparedStatement.executeQuery();
+		resultSet.getString("username");
+		resultSet.getString("firstname");
+		resultSet.getString("lastname");
+
+		Class<User> clazz = User.class;
+		Constructor<User> constructor = clazz.getConstructor();
+		User user = constructor.newInstance();
+		Field usernameField = clazz.getDeclaredField("username");
+		usernameField.setAccessible(true);
+		usernameField.set(user, resultSet.getString("username"));
+
+
+	}
+	@Test
+	void checkReflectionApi() throws SQLException, IllegalAccessException {
+		User user = User.builder()
+				.build();
+
+		String sql = """
+				insert
+				into 
+				%s
+				(%s)
+				values
+				(%s)
+				""";
+
+
+		String tableName = Optional.ofNullable(user.getClass().getAnnotation(Table.class))
+				.map(tableAnnotation -> tableAnnotation.schema() + "." + tableAnnotation.name())
+				.orElse(user.getClass().getName());
+
+		Field[] declaredFields = user.getClass().getDeclaredFields();
+
+		String columnNames = Arrays.stream(declaredFields)
+				.map(field -> Optional.ofNullable(field.getAnnotation(Column.class))
+						.map(Column::name)
+						.orElse(field.getName()))
+				.collect(Collectors.joining(", "));
+
+		String columnValues = Arrays.stream(declaredFields)
+				.map(field -> "?")
+				.collect(Collectors.joining(", "));
+
+		System.out.println(sql.formatted(tableName,columnNames,columnValues));
+
+		Connection connection = null;
+		PreparedStatement preparedStatement = connection.prepareStatement(sql.formatted(tableName, columnNames, columnValues));
+		for (Field declaredField : declaredFields) {
+			declaredField.setAccessible(true);
+			preparedStatement.setObject(1, declaredField.get(user));
+		}
+
+
+	}
+
+}
