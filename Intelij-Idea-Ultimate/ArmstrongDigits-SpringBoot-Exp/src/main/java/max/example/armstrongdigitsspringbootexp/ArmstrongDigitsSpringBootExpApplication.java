81d15eb97e9b2b15f01c3d0d47954e417918a0b3
MaximButmanov <but-maxim@yandex.ru>
2022-11-29 16:11:07 +0300

    init commit

---
+++ Intelij-Idea-Ultimate/ArmstrongDigits-SpringBoot-Exp/src/main/java/max/example/armstrongdigitsspringbootexp/ArmstrongDigitsSpringBootExpApplication.java
@@ -0,0 +1,13 @@
+package max.example.armstrongdigitsspringbootexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmstrongDigitsSpringBootExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArmstrongDigitsSpringBootExpApplication.class, args);
+    }
+
+}
