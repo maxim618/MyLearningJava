80d5292a3bb00de1e65568f89f7c8ed07ad7a5ca
MaximButmanov <but-maxim@yandex.ru>
2022-12-02 16:13:35 +0300

    init commit

---
+++ Intelij-Idea-Ultimate/ArmstrongDigits-SpringBoot-Exp/src/test/java/max/example/armstrongdigitsspringbootexp/ArmstrongDigitsSpringBootExpApplicationTests.java
@@ -0,0 +1,13 @@
+package max.example.armstrongdigitsspringbootexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmstrongDigitsSpringBootExpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
