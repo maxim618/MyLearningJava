90c97775357740aa3c485c72c13cfd045e2181d2
MaximButmanov <but-maxim@yandex.ru>
2022-12-11 16:18:41 +0300

    init commit

---
+++ LambdaExperience/src/main/java/Animal.java
@@ -0,0 +1,18 @@
+public class Animal {
+    private String species;
+    private boolean canHop;
+    private boolean canSwim;
+
+    public Animal(String species, boolean canHop, boolean canSwim) {
+        this.species = species;
+        this.canHop = canHop;
+        this.canSwim = canSwim;
+    }
+    public boolean canHop() {return canHop;}
+    public boolean CanSwim(){return  canSwim;}
+
+    @Override
+    public String toString() {
+        return species;
+    }
+}
