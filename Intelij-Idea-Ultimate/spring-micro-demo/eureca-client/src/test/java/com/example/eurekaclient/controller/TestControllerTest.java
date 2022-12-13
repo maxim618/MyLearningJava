d2ecf05c3c57aa10df2ca55c376c732cc8433aa5
MaximButmanov <but-maxim@yandex.ru>
2022-12-13 16:19:32 +0300

    init commit

---
+++ Intelij-Idea-Ultimate/spring-micro-demo/eureca-client/src/test/java/com/example/eurekaclient/controller/TestControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.eurekaclient.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(TestController.class)
+public class TestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testTestWhenCalledThenReturnTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("test"));
+    }
+}
