5b5e1ec2964b7ea480cb3c5f9f89caffefd80e4e
MaximButmanov <but-maxim@yandex.ru>
2023-01-14 12:23:12 +0300

    init commit

---
+++ BankCardManagementSystem/src/main/java/max/example/bankcardmanagementsystem/BankCardManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package max.example.bankcardmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCardManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankCardManagementSystemApplication.class, args);
+    }
+
+}
