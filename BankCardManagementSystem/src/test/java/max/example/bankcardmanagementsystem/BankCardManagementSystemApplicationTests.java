0289c8cd8de5f22dda2df2abf9ab0b76ce4d21f0
MaximButmanov <but-maxim@yandex.ru>
2023-01-16 12:27:02 +0300

    add BankCardManagementSystemApplicationTests.java

---
+++ BankCardManagementSystem/src/test/java/max/example/bankcardmanagementsystem/BankCardManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package max.example.bankcardmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCardManagementSystemApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
