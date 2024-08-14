15d581836fd2c29916e267b9c1ad805eb39765b0
MaximButmanov <but-maxim@yandex.ru>
2024-08-14 18:09:52 +0300

    commit

---
+++ Flight2/src/test/java/Experience/WithoutDepartureBeforeArrivalTest.java
@@ -0,0 +1,15 @@
+package Experience;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WithoutDepartureBeforeArrivalTest {
+
+
+
+    @Test
+    void filtering() {
+        WithoutDepartureBeforeArrival withoutDepartureBeforeArrival = new WithoutDepartureBeforeArrival();
+    }
+}
